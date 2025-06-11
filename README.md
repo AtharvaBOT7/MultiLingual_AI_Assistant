c6b51961f510415019275e6097cf84dd4bc83d25
AtharvaBOT7 <atharvachundurwar841@gmail.com>
2025-06-11 16:44:50 -0700

    New Project folder structure added

---
+++ README.md
@@ -0,0 +1,4 @@
+Virtual Environment used: AIassistant
+
+To activate the virtual environment:
+conda activate /Users/atharva7/Downloads/MultiLingual_AI_Assistant/AIassistant
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="AI Virtual Assistant",
+      version="0.0.1",
+      author="Atharva Chundurwar",
+      author_email="atharvachundurwar841@gmail.com",
+      packages=find_packages()
+)
